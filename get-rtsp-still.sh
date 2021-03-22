f88dc4e7db3e57bfd27d368f272e2f36f6ed95ca
mathew-fleisch <mathew.fleisch@gmail.com>
2021-03-22 08:43:17 -0700

    Add POC rtsp still

---
+++ get-rtsp-still.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "$(command -v ffmpeg)" ]]; then
+	echo "Installing ffmpeg..."
+	apt update && apt install -y ffmpeg
+fi
+
+rtsp_stream="{1:-rtsp://10.10.10.136:8000/stream}"
+target_filename="frontdoor-$(date +'%F-%H-%M-%S').jpg"
+ffmpeg -hide_banner -loglevel error -y -i $rtsp_stream -vframes 1 $target_filename
+
+# Channel id
+target_channels="{2:-C01RUPNRN78}"
+
+curl -F file=@"$target_filename" -F "initial_comment=$(date)" -F channels=$target_channels -H "Authorization: Bearer $SLACK_TOKEN" https://slack.com/api/files.upload
+
