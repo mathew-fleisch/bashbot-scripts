fce08c8124e9bd5f21e13af8a2dc079465522cd6
mathew-fleisch <mathew.fleisch@gmail.com>
2021-03-22 10:29:31 -0700

    fix POC

--- get-rtsp-still.sh
+++ get-rtsp-still.sh
@@ -5,12 +5,12 @@ if [[ -z "$(command -v ffmpeg)" ]]; then
 	apt update && apt install -y ffmpeg
 fi
 
-rtsp_stream="{1:-rtsp://10.10.10.136:8000/stream}"
+rtsp_stream="rtsp://10.10.10.136:8000/stream"
 target_filename="frontdoor-$(date +'%F-%H-%M-%S').jpg"
 ffmpeg -hide_banner -loglevel error -y -i $rtsp_stream -vframes 1 $target_filename
 
 # Channel id
-target_channels="{2:-C01RUPNRN78}"
+target_channels="C01RUPNRN78"
 
-curl -F file=@"$target_filename" -F "initial_comment=$(date)" -F channels=$target_channels -H "Authorization: Bearer $SLACK_TOKEN" https://slack.com/api/files.upload
+curl -s -F file=@"$target_filename" -F "initial_comment=$(date)" -F channels=$target_channels -H "Authorization: Bearer $SLACK_TOKEN" https://slack.com/api/files.upload
 
