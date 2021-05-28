f27807f2addda45d07fe31fa49c3306a91ad8fc5
mathew-fleisch <mathew.fleisch@gmail.com>
2021-05-28 15:04:08 -0700

    Add POC for trivia game

---
+++ trivia-game.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+trivia_yaml=${1:-friends.yaml}
+#C01RUPNRN78
+
+# Total number of questions
+num_questions=$(yq e '.[].q' $trivia_yaml | sort | wc -l)
+
+# Random question index
+random_question=$(shuf -i 1-$num_questions -n 1)
+random_index=$((random_question-1))
+
+# Question
+question=$(yq e '.['$random_index'].q' $trivia_yaml)
+
+# Answer
+answer=$(yq e '.['$random_index'].a' $trivia_yaml)
+
+# Meta-Data
+# metadata=$(yq e '.['$random_index'].d' $trivia_yaml)
+
+# echo "$metadata: $question"
+# echo "$answer"
+
+response=$(./slackApi.sh \
+    --slack-token $SLACK_TOKEN \
+    --slack-channel $TRIGGERED_CHANNEL_ID \
+    --endpoint chat.postMessage \
+    --message "$question")
+thread=$(echo "$response" | jq -r '.ts')
+sleep 1
+./slackApi.sh \
+    --slack-token $SLACK_TOKEN \
+    --slack-channel $TRIGGERED_CHANNEL_ID \
+    --endpoint chat.postMessage \
+    --thread-ts $thread \
+    --message "$answer"
+
+
+
